6d3d89ece282ff9d6e2bf3c735e6d8d881b1ff97
Grzelkos <127422252+GrzegorzWawruch@users.noreply.github.com>
2024-08-15 11:23:51 +0200

    Add files via upload

---
+++ LoginDate.ps1
@@ -0,0 +1,42 @@
+﻿#Skrypt which get back last date of login users to domain
+
+cls
+
+$while = $true
+$ADUsers = @()
+
+while($while -eq $true)
+{
+    Write-Host "Enter the Domain User name or if you want to get information about last login date of all domain users type 'all':  " -NoNewline
+    $HMUsers = Read-Host
+
+    $ADUsers = Get-ADUser -Filter * -Properties 'Name', 'LastLogonDate' 
+
+    if($HMUsers -eq 'all')
+    {      
+        $usersInfo = foreach($item2 in $ADUsers)
+        {
+            [PSCustomObject]@{
+                UserName       = $item2.Name
+                LastLogonDate  = if ($item2.LastLogonDate) { $item2.LastLogonDate } else { "N/A" }
+            }
+        }
+        $usersInfo
+        $while = $false
+        break
+    }
+    else
+    {
+        $User = Get-ADUser -Filter "Name -eq '$HMUsers'" -Properties 'Name', 'LastLogonDate'
+        if($HMUsers -eq $User.Name)
+        {
+            'User ' + $User.Name + ' last logon date ' + $User.LastLogonDate
+            
+            $while = $false
+        }    
+        else
+        {
+            Write-Host "You put $HMUsers and it is wrong answer, try again !"
+        }
+    }
+}
